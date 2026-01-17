20fb6350699ece75974a49bdcbb357b41f3bb707
DocPis <docpis@tutamail.com>
2026-01-17 14:20:53 +0100

    a

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -10,7 +10,7 @@ const baseUrl = process.env.BASE_URL ?? '/';
 const config: Config = {
   title: 'CurrentX',
   tagline: 'Docs for the CurrentX DEX on MegaETH',
-  favicon: 'img/currentxlogo.png',
+  favicon: 'img/favicon.ico',
 
   // Future flags, see https://docusaurus.io/docs/api/docusaurus-config#future
   future: {
